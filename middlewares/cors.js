6a8b79c427b36c2ae35328c77f7bb72042ff4c46
k0Daniel <Daniil.k0sarev@yandex.ru>
2024-05-08 14:39:59 +0300

    upd: cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,9 @@
-const allowedCors = ['https://practicum.yandex.ru', 'https://students-projects.ru', 'localhost:3000', 'localhost:3001'];
+const allowedCors = [
+	'https://practicum.yandex.ru',
+	'https://students-projects.ru',
+	'http://localhost:3000',
+	'http://localhost:3001'
+];
 
 function cors(req, res, next) {
 	const { origin } = req.headers;
@@ -6,7 +11,6 @@ function cors(req, res, next) {
 	if (allowedCors.includes(origin)) {
 		res.header('Access-Control-Allow-Origin', origin);
 	}
-
 	next();
 }
 
