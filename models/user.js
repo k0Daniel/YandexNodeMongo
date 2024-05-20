861d15cbe38c98750cf60ce78d834b12cc32fef0
k0Daniel <Daniil.k0sarev@yandex.ru>
2024-05-20 23:03:31 +0300

    small fix

--- models/user.js
+++ models/user.js
@@ -18,25 +18,20 @@ const userSchema = new mongoose.Schema({
 	}
 });
 
+userSchema.statics.findUserByCredentials = function (email, password) {
+	return this.findOne({ email }).then(user => {
+		if (!user) {
+			return Promise.reject(new Error('Неправильные почта или пароль'));
+		}
 
-userSchema.statics.findUserByCredentials = function(email, password) {
-  return this.findOne({ email })
-    .then(user => {
-      if (!user) {
-        return Promise.reject(new Error("Неправильные почта или пароль"));
-      }
+		return bcrypt.compare(password, user.password).then(matched => {
+			if (!matched) {
+				return Promise.reject(new Error('Неправильные почта или пароль'));
+			}
 
-      return bcrypt.compare(password, user.password)
-        .then(matched => {
-          if (!matched) {
-            return Promise.reject(new Error("Неправильные почта или пароль"));
-          }
-
-          return user;
-      });
-  });
+			return user;
+		});
+	});
 };
-
-module.exports = mongoose.model("user", userSchema);
 
 module.exports = mongoose.model('user', userSchema);
